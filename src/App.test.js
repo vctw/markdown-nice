a70c0489e341cbe37472fb6d998c7e6b7d6612dc
vct <victorw@aliyun.com>
2021-02-24 01:37:55 +0800

    jest init

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test("object assignment", () => {
+  const data = {one: 1};
+  data["two"] = 2;
+  expect(data).toEqual({one: 1, two: 2});
 });
